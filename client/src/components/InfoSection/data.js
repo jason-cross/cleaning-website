4d6e61f7a8885d61957c9aec7d8e6f596646ba75
Jason Cross <jasoncross1357@gmail.com>
2022-07-20 01:28:36 +0800

    added page element and about page

---
+++ client/src/components/InfoSection/data.js
@@ -0,0 +1,21 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'About Me',
+    headLine: `Hi I'm Lia`,
+    description: `I offer affordable, general and deep cleaning to all households, 
+        whether it be a new home, your rental property or owned home, I understand 
+        that the place you reside is one of your biggest livelihoods and 
+        appreciate that keeping on top of daily cleaning isn't attainable for so 
+        many and wish to provide a service that helps you to manage that side of 
+        life so you can spend more time on what matters most to you.`,
+    buttonLabel: 'Read more',
+    imgStart: false,
+    img: require('../../assets/images/about.png'),
+    alt: 'Cleaning',
+    dark: true,
+    primary: true,
+    darkText: false
+};
