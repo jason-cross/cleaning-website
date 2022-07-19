1b0d6355f79f1350eccb3a2a15ffb6daef5cfd3a
Jason Cross <jasoncross1357@gmail.com>
2022-07-19 20:51:14 +0800

    added sidebar for mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './pages';
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
+
+const Home = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+            <Navbar toggle={toggle}/>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+        </>
+    );
+};
+
+export default Home;
