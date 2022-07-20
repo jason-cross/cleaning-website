1c376f0f55c7ddf02af82a856687e665fbc8b040
Jason Cross <jason.cross1@student.curtin.edu.au>
2022-07-20 11:58:24 +0800

    fixed services page height

--- client/src/components/Services/ServicesElements.js
+++ client/src/components/Services/ServicesElements.js
@@ -1,20 +1,22 @@
 import styled from 'styled-components';
 
 export const ServicesContainer = styled.div`
-    height: 800px;
+    height: auto;
+    padding-top: 10%;
+    padding-bottom: 10%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     background: #010606;
 
-    @media screen and (max-width: 768px) {
-        height: 1100px;
-    }
+    /* @media screen and (max-width: 768px) { */
+        /* height: 1100px; */
+    /* } */
 
-    @media screen and (max-width: 480px) {
-        height: 1300px;
-    }
+    /* @media screen and (max-width: 480px) { */
+        /* height: 1300px; */
+    /* } */
 `;
 
 export const ServicesWrapper = styled.div`
